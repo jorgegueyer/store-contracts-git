9110801cbfcbe1178212c5a18d400aa84979b3a3
Jorge Guerra Yerpes <jorgegueyer@gmail.com>
2019-04-01 13:04:25 +0200

    Structure and some contracts for consumer-contract

---
+++ META-INF/com/jgy/producer-contract/0.0.1-SNAPSHOP/contracts/consumer-contract/shouldReturnOddWhenRequestParamIsOdd.groovy
@@ -0,0 +1,18 @@
+package resources.contracts
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return odd when number input is odd - tests"
+    request {
+        method GET()
+        url("/validate/prime-number") {
+            queryParameters {
+                parameter("number", "1")
+            }
+        }
+    }
+    response {
+        body("Odd")
+        status 200
+    }
+}
